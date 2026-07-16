d232a36e6e75ba4c6e10307dbf3b43218b6aa452
Kronos <138235386+RealKronos69@users.noreply.github.com>
2026-07-16 17:18:13 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (59.69%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let i=0
+    let j=nums.length-1
+    while (i<=j) {
+        let mid = Math.floor((i+j)/2)
+        if (target === nums[mid]) {
+            return mid
+        }
+        if (target<nums[mid]) {
+            j = mid-1
+        }else if(target>nums[mid]){
+            i = mid+1
+        }
+    }
+    return i
+
+    return nums.length
+};
